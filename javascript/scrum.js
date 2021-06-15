1279b29b318c4202a47f9587181a4df7ffe161ba
wallacy <wwallacy@Live.com>
2021-06-15 15:34:19 -0300

    primeiro commit

---
+++ javascript/scrum.js
@@ -0,0 +1,63 @@
+// help
+function log(message) {
+    console.log('> ' + message)
+
+}
+
+// app
+const cards = document.querySelectorAll('.card')
+const dropzones = document.querySelectorAll('.dropzone')
+
+// cards
+cards.forEach(card =>{
+    card.addEventListener('dragstart', dragstart)
+    card.addEventListener('drag', drag)
+    card.addEventListener('dragend', dragend)
+})
+
+function dragstart() {
+    dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
+
+    this.classList.add('is-dragging')
+}
+
+function drag() {
+}
+
+function dragend() {
+    dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
+    //this = card
+    this.classList.remove('is-dragging')
+
+}
+
+// local onde vamos dropar os cards
+dropzones.forEach( dropzone =>{
+    dropzone.addEventListener('dragenter', dragenter)
+    dropzone.addEventListener('dragover', dragover)
+    dropzone.addEventListener('dragleave', dragleave)
+    dropzone.addEventListener('drop', drop)
+})
+
+function dragenter(){
+}
+
+function dragover(){
+    this.classList.add('over');
+
+    // get draggin card
+    const cardBeingDragged = document.querySelector('.is-dragging')
+    //this = dropzone
+    this.appendChild(cardBeingDragged)
+
+}
+
+function dragleave() {
+    //this = card
+    this.classList.remove('over');
+}
+
+function drop() {
+    this.classList.remove('over');
+
+}
